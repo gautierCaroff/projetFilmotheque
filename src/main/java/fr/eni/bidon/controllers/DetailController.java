5999fc7481b6b71ce6f9c51bcdd14c777c1e7f84
gautiercaroff <gautier.caroff2023@campus-eni.fr>
2023-08-10 15:13:51 +0200

    ListController pour premier test

--- src/main/java/fr/eni/bidon/controllers/DetailController.java
+++ src/main/java/fr/eni/bidon/controllers/DetailController.java
@@ -2,10 +2,12 @@ package fr.eni.bidon.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import fr.eni.bidon.bll.FilmService;
+import fr.eni.bidon.bo.Film;
 
 @Controller
 public class DetailController {
@@ -18,19 +20,12 @@ public class DetailController {
 	}
 	
 	@GetMapping
-	public String afficherDetail(@RequestParam int idFilm) {
-		
-		 
-		
-		
+	public String afficherDetail(@RequestParam int idFilm, Model modele) {
 		
 		// allez chercher le film
-		
-		
-		
+		Film film = filmeService.findById(idFilm);
 		// le metre dans le model
-		
-		
+		modele.addAttribute("film", film);
 		
 		return "detail";
 	}
--- src/main/java/fr/eni/bidon/controllers/ListeController.java
+++ src/main/java/fr/eni/bidon/controllers/ListeController.java
@@ -1,13 +1,29 @@
 package fr.eni.bidon.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import fr.eni.bidon.bll.FilmService;
+import fr.eni.bidon.bo.Film;
 
 @Controller
 public class ListeController {
+	
+	private FilmService filmeService; 
+	
+	@Autowired
+	public ListeController(FilmService filmeService) {
+		this.filmeService = filmeService;
+	}
 
     @GetMapping({"/","/liste"})
-    public String afficherDetail(){
+    public String afficherDetail(Model modele){
+    	List<Film> films = filmeService.findList();
+    	modele.addAttribute(films);
 
         return "liste";
     }
